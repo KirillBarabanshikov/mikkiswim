0bc46aaad589f170dc1705e23ba6d293c5d12b11
vlad <vladkoip13@gmail.com>
2025-04-05 16:55:46 +0400

    style: b2bbasket components

---
+++ share/components/b2b-basket/B2BBasketItem/B2BBasketItem.vue
@@ -0,0 +1,156 @@
+<script setup lang="ts">
+import { priceFormatter } from '~/share/utils/priceFormatter'
+
+defineProps<{
+  item: any
+  apiUrl: string
+}>()
+</script>
+
+<template>
+  <div class="basket-item">
+    <div class="basket-item__pic">
+      <img
+        :src="apiUrl + item.product.images[0].image"
+        alt=""
+        width="115"
+        height="150"
+      />
+      <div class="basket-item__pic-quantity">
+        {{ item.quantity }}
+      </div>
+    </div>
+    <div class="basket-item__body">
+      <div class="basket-item__row">
+        <div class="basket-item__title">
+          {{ item.product.catalogs[0].title }}
+          <b>{{ item.product.title }}</b>
+        </div>
+        <div class="basket-item__actions">
+          <IconEdit />
+          <IconDelete />
+        </div>
+      </div>
+      <div class="basket-item__row">
+        <div class="basket-item__details">
+          <div class="basket-item__details-item">
+            <div class="basket-item__details-item__label">Розница</div>
+            <div class="basket-item__details-item__value">
+              {{ priceFormatter(item.product.price) }}
+            </div>
+          </div>
+          <div class="basket-item__details-item">
+            <div class="basket-item__details-item__label">B2B</div>
+            <div class="basket-item__details-item__value">
+              {{ priceFormatter(item.product.priceB2B) }}
+            </div>
+          </div>
+          <div class="basket-item__details-item">
+            <div class="basket-item__details-item__label">Размер</div>
+            <div class="basket-item__details-item__value">
+              {{ item.size }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.basket-item {
+  width: 100%;
+  display: flex;
+  gap: 22px;
+
+  &__pic {
+    position: relative;
+    flex-shrink: 0;
+
+    img {
+      border-radius: 8px;
+      object-fit: contain;
+      display: block;
+    }
+
+    &-quantity {
+      position: absolute;
+      bottom: 8px;
+      left: 8px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: var(--white);
+      border-radius: 8px;
+      padding: 0 8px;
+      width: 34px;
+      height: 22px;
+      font-weight: 700;
+      font-size: 14px;
+      line-height: 155%;
+    }
+  }
+
+  &__body {
+    padding: 10px;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+  }
+
+  &__title {
+    line-height: 155%;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    font-size: 14px;
+
+    b {
+      font-weight: 700;
+      font-size: 16px;
+    }
+  }
+
+  &__row {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  &__actions {
+    display: flex;
+    gap: 32px;
+
+    &:deep(svg) {
+      cursor: pointer;
+      color: var(--gray);
+      width: 23px;
+      height: 23px;
+    }
+  }
+
+  &__details {
+    line-height: 155%;
+    display: flex;
+    gap: 32px;
+
+    &-item {
+      flex: 0 0 114px;
+      display: flex;
+      flex-direction: column;
+      gap: 4px;
+
+      &__value {
+        font-weight: 700;
+        font-size: 16px;
+      }
+
+      &__label {
+        color: var(--gray);
+        font-size: 14px;
+      }
+    }
+  }
+}
+</style>
---
+++ share/components/b2b-basket/B2BBasketTotals/B2BBasketTotals.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import { priceFormatter } from '~/share/utils/priceFormatter'
+
+defineProps<{
+  totalQuantity: number
+  totalWeight: number
+  totalPrice: number
+}>()
+</script>
+
+<template>
+  <div class="basket-totals">
+    <div class="basket-totals-row">
+      <div class="basket-totals__item">
+        <div class="basket-totals__item-label">Количество, шт.</div>
+        <div class="basket-total__item-value">
+          {{ totalQuantity }}
+        </div>
+      </div>
+      <div class="basket-totals__item">
+        <div class="basket-totals__item-label">Вес, гр</div>
+        <div class="basket-totals__item-value">{{ totalWeight }}</div>
+      </div>
+    </div>
+    <div class="basket-totals-row">
+      <div class="basket-totals-row__label">Итого</div>
+      <div class="basket-totals-row__value">
+        {{ priceFormatter(totalPrice) }}
+      </div>
+    </div>
+    <Button>Продолжить</Button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.basket-totals {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  line-height: 155%;
+
+  &-row {
+    display: flex;
+    justify-content: space-between;
+
+    &:last-child {
+      border-top: 1px solid var(--gray-400);
+      padding-top: 21px;
+      font-weight: 700;
+      font-size: 20px;
+    }
+  }
+
+  &__item {
+    display: flex;
+    flex-direction: column;
+    gap: 6px;
+
+    &:nth-child(2) {
+      .basket-totals__item-value {
+        text-align: right;
+      }
+    }
+
+    &-label {
+      font-weight: 700;
+      font-size: 16px;
+    }
+
+    &-value {
+      font-size: 16px;
+    }
+  }
+
+  button {
+    margin: 0 auto;
+    min-width: 261px;
+  }
+}
+</style>
